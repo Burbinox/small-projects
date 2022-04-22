adf083ae5a10d7a7692409fde58317f7f61a6b9a
Burbinox <hubert.grobelny@interia.pl>
2022-04-22 08:57:58 +0200

    update sum_of_numbers_in_triangle to calculate the longest path and its sum

--- sum_of_numbers_in_a_triangle/optimal_solution.py
+++ sum_of_numbers_in_a_triangle/optimal_solution.py
@@ -34,10 +34,38 @@ def find_the_shortest_path(triangle):
     print("the sum of the shortest path is: " + str(triangle[0][0]))
 
 
-with open('a68617c92920fe68c777d389e25e7d4b/3-medium.txt') as f:
+def find_the_longest_path(triangle):
+    triangle.reverse()
+    print(triangle)
+    print()
+    longest_path = []
+    for floor in range(len(triangle)):  # floor indexes
+        if floor + 1 == len(triangle):
+            break
+        for elem in range(len(triangle[floor + 1])):  # elements in before the last one floor
+            if floor == 0:
+                path = str(max(triangle[floor][elem], triangle[floor][elem + 1])) + str(triangle[floor + 1][elem])
+                longest_path.append(path)
+                path = 0
+            else:
+                first_elem = triangle[floor][elem]
+                second_elem = triangle[floor][elem + 1]
+                if first_elem > second_elem:
+                    longest_path[elem] = str(longest_path[elem]) + str(triangle[floor + 1][elem])
+                else:
+                    longest_path[elem] = str(longest_path[elem + 1]) + str(triangle[floor + 1][elem])
+            triangle[floor + 1][elem] = triangle[floor + 1][elem] + max(triangle[floor][elem],
+                                                                        triangle[floor][elem + 1])
+    print("the longest path is: " + longest_path[0][::-1])
+    triangle.reverse()
+    print("the sum of the longest path is: " + str(triangle[0][0]))
+
+
+with open('examples/3-medium.txt') as f:
     tri = f.readlines()
     tri = [elem.strip() for elem in tri]
     tri = [elem.replace(" ", "") for elem in tri]
 
     new_tri = change_data_format_to_lists_of_list(tri)
-    find_the_shortest_path(new_tri)
+    find_the_longest_path(new_tri)
+    # find_the_shortest_path(new_tri)
